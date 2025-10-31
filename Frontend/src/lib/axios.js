33b70ac40178880737d5838ed4a047b1ace1d89d
divyagoyal3 <goyaldivya631@gmail.com>
2025-10-31 16:09:51 +0530

    Update axios baseURL for production

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseURL =
+const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL;
+    : "https://chat-app-mern-3-jhvh.onrender.com/api"; // your backend Render link
 
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL: BASE_URL,
   withCredentials: true,
 });
